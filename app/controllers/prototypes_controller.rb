e29ba472e2aaaf36ebb510a9f7d013c428fb6ae3
kurosawa1109 <88430509+kurosawa1109@users.noreply.github.com>
2021-08-27 17:36:58 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,62 @@
+class PrototypesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_protopace, except: [:index,:new,:create]
+  before_action :move_to_index, only: [:shoe, :update]
+
+  
+  def index
+    @prototype = Prototype.all
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+  end
+
+  def edit
+    
+  end
+
+  def update
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @prototype.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  # ここで@prototypeを定義してshow、edit、update、destroyに対しbefore_actionで読み込んでいる。
+  def set_protopace
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in? && current_user.id == @prototype.user_id
+      redirect_to action: :index
+    end
+  end
+end
